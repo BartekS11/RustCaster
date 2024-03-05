65183d227536c5eca8d2ca3495cafa6c9ed71400
BartekS11 <marekx71@gmail.com>
2024-03-06 00:03:03 +0100

    add sample bevy related test

--- tests/world_tests.rs
+++ tests/world_tests.rs
@@ -1,8 +1,18 @@
-use bevy::app::App;
+use bevy::app::{App, Update};
+use bevy::prelude::*;
+
+
+fn spawn_mock_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::new_with_far(1.0));
+}
 
 #[test]
 fn did_app() {
     let mut app = App::new();
 
-    assert_eq!(true, true);
+    app.add_systems(Update, spawn_mock_camera);
+
+    app.update();
+
+    assert_eq!(app.world.query::<&Camera2d>().iter(&app.world).len(), 1);
 }
