19754a12f493f5c6f985ed1995d9938dc5ded777
BartekS11 <marekx71@gmail.com>
2024-03-11 00:09:02 +0100

    add module raycasting

---
+++ src/raycasting/mod.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+
+// use self::rays::raycaster;
+
+pub mod rays;
+pub mod rays_constants;
+pub struct RaycastingPlugin;
+
+impl Plugin for RaycastingPlugin {
+    fn build(&self, app: &mut App) {
+        // app.add_systems(Update, raycaster);
+    }
+}
