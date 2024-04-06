a8e3273739cc443e2638782a6e07bfa60fbd8f66
BartekS11 <marekx71@gmail.com>
2024-04-06 22:40:50 +0200

    fix

--- src/player/player_constants.rs
+++ src/player/player_constants.rs
@@ -1,6 +1,6 @@
 use bevy::{math::Vec2, render::color::Color};
 
-pub const PLAYER_SPEED: f32 = 1.8;
+pub const PLAYER_SPEED: f32 = 1.85;
 pub const PLAYER_ROTATING_SPEED: f32 = 0.05;
 pub const PLAYER_STARTING_POSITION: Vec2 = Vec2::new(1.5, 1.5);
 pub const PLAYER_STARTING_ROTATION: f32 = 0.0;
